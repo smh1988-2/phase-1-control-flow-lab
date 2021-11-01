3a070135441cac3641bf3b91fe68e4b3abe4ee75
smh1988-2 <seamhurley1988@gmail.com>
2021-11-01 15:19:35 -0700

    commit tree ee911cdc8bba169b85ceaa8c0527d158f4460574

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    return "This one is on me!";
+  } else if (distance >= 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (distance > 2500) {
+    return "No can do."
+  }
+};
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+
+
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ switch(tip) {
+ case "generous":
+ return "Thank you so much.";
+ break;
+ case "not as generous":
+ return "Thank you.";
+ break;
+ case "thanks for everything":
+ return "Bye.";
+ break;
+}
 }
